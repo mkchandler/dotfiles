c4bdcc1fd4517132cc5af356f73c6af85d404675
Matt Chandler <mkchandler@gmail.com>
2017-03-14 21:24:50 -0500

    Remove brews that I don't use

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -12,7 +12,7 @@ brew update
 brew install coreutils findutils
 
 # Install Bash 4 and git
-brew install bash git hub shellcheck
+brew install bash git
 
 # Install proggy langs
 brew install go elixir nvm python scala
@@ -24,11 +24,11 @@ brew install redis rethinkdb sqlite
 brew tap homebrew/dupes
 brew install homebrew/dupes/grep homebrew/dupes/rsync homebrew/dupes/nano
 brew install homebrew/dupes/whois homebrew/dupes/unzip homebrew/dupes/less homebrew/dupes/gzip
-brew install homebrew/dupes/awk homebrew/dupes/bzip2 homebrew/dupes/diffstat homebrew/dupes/diffutils homebrew/dupes/screen
+brew install homebrew/dupes/awk homebrew/dupes/bzip2 homebrew/dupes/diffstat homebrew/dupes/diffutils
 
-brew install gpg2 curl wget tmux ssh-copy-id speedtest_cli
+brew install gpg2 curl wget tmux ssh-copy-id
 
-# Media lib to fix apple buffoonery 
+# Media lib to fix apple buffoonery
 brew install libdvdcss
 
 brew linkapps
