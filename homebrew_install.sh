e526da8ab6f9156c5e7422dbda6a968ee5b7eb87
Matt Chandler <mkchandler@gmail.com>
2018-01-31 19:00:22 -0600

    Add cloud and testing stuff and other updates to brew file

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -11,14 +11,20 @@ brew update
 # Install GNU core utilities (those that come with OS X are outdated)
 brew install coreutils findutils
 
-# Install Bash 4 and git
-brew install bash git
+# Install dev tools
+brew install bash git jq tmux
 
 # Install proggy langs
 brew install go elixir nvm python scala
 
 # Install data stuff
-brew install redis rethinkdb sqlite
+brew install redis sqlite
+
+# Install testing stuff
+brew install chromedriver
+
+# Install cloud stuff
+brew install heroku terraform awscli
 
 # Install more recent versions of some OS X tools
 brew tap homebrew/dupes
@@ -26,7 +32,8 @@ brew install homebrew/dupes/grep homebrew/dupes/rsync homebrew/dupes/nano
 brew install homebrew/dupes/whois homebrew/dupes/unzip homebrew/dupes/less homebrew/dupes/gzip
 brew install homebrew/dupes/awk homebrew/dupes/bzip2 homebrew/dupes/diffstat homebrew/dupes/diffutils
 
-brew install gpg2 curl wget tmux ssh-copy-id
+# Other stuff
+brew install gpg2 curl wget ssh-copy-id
 
 # Media lib to fix apple buffoonery
 brew install libdvdcss
